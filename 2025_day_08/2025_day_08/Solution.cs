a1a15a5c9c0b41f5373eb65f5152c5f182b22c37
Dániel Marcell <daniel.marcell171@gmail.com>
2025-12-08 16:40:01 +0100

    day 8 template

---
+++ 2025_day_08/2025_day_08/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_day_08 {
+    internal class Solution {
+        public uint Result_PartOne;
+        public uint Result_PartTwo;
+        public Solution() {
+            string lineOfText;
+            string ConfigPath = AppDomain.CurrentDomain.BaseDirectory + "input.txt";
+            int lineIndex = 0;
+            FileStream filestream = new FileStream(ConfigPath,
+                                          System.IO.FileMode.Open,
+                                          System.IO.FileAccess.Read,
+                                          System.IO.FileShare.ReadWrite);
+            var reader = new System.IO.StreamReader(filestream, System.Text.Encoding.UTF8, true, 128);
+            string[] lineArray;
+            bool firstline = true;
+            int Row = 0;
+            while ((lineOfText = reader.ReadLine()) != null) {
+                if (firstline) {
+                    firstline = false;
+                    //MatrixSize = lineOfText.Count();
+                    //initialize input
+                    //Input = new Tile[MatrixSize, MatrixSize];
+                }
+                int Col = 0;
+                foreach (char c in lineOfText) {
+                    //Input[Row, Col] = new Tile();
+                    //Input[Row, Col].Type = c;
+                    //Col++;
+                }
+                Row++;
+            }
+
+        }
+
+        public void PartOne() {
+        }
+        public void PartTwo() {
+        }
+    }
+}
